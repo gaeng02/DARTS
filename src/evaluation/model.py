82e2555059af3a83d9716836508d74ec230d3ed0
gaeng02 <sondin144@gmail.com>
2024-12-01 17:04:41 +0900

    considered 3rd path

--- src/evaluation/model.py
+++ src/evaluation/model.py
@@ -17,46 +17,76 @@ class Cell(nn.Module):
       self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0)
     self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0)
 
-    if reduction:
-      op_names, indices = zip(*genotype.reduce)
+    # genotype changed
+    # node, PRIMITIVE, j, weight
+    if reduction :
+      nodes, op_names, indices, weights = zip(*genotype.reduce)
       concat = genotype.reduce_concat
-    else:
-      op_names, indices = zip(*genotype.normal)
+    else :
+      nodes, op_names, indices, weights = zip(*genotype.normal)
       concat = genotype.normal_concat
-    self._compile(C, op_names, indices, concat, reduction)
+    self._compile(C, nodes, op_names, indices, weights, concat, reduction)
 
-  def _compile(self, C, op_names, indices, concat, reduction):
+  def _compile(self, C, nodes, op_names, indices, weights, concat, reduction) :
+    
     assert len(op_names) == len(indices)
-    self._steps = len(op_names) // 2
+
+    # hard coded
+    self._steps = 4 # range (2, 6)
     self._concat = concat
     self.multiplier = len(concat)
 
     self._ops = nn.ModuleList()
-    for name, index in zip(op_names, indices):
+    for name, index in zip(op_names, indices) :
       stride = 2 if reduction and index < 2 else 1
       op = OPS[name](C, stride, True)
       self._ops += [op]
     self._indices = indices
 
-  def forward(self, s0, s1, drop_prob):
+  def forward(self, s0, s1, drop_prob) :
     s0 = self.preprocess0(s0)
     s1 = self.preprocess1(s1)
 
     states = [s0, s1]
-    for i in range(self._steps):
-      h1 = states[self._indices[2*i]]
-      h2 = states[self._indices[2*i+1]]
-      op1 = self._ops[2*i]
-      op2 = self._ops[2*i+1]
-      h1 = op1(h1)
-      h2 = op2(h2)
-      if self.training and drop_prob > 0.:
-        if not isinstance(op1, Identity):
-          h1 = drop_path(h1, drop_prob)
-        if not isinstance(op2, Identity):
-          h2 = drop_path(h2, drop_prob)
-      s = h1 + h2
+    
+    for i in range(self._steps) :
+      if (i == 0) :
+        # not changed 
+        h1 = states[self._indices[2*i]]
+        h2 = states[self._indices[2*i+1]]
+        op1 = self._ops[2*i]
+        op2 = self._ops[2*i+1]
+        h1 = op1(h1)
+        h2 = op2(h2)
+        if self.training and drop_prob > 0. :
+          if not isinstance(op1, Identity) :
+            h1 = drop_path(h1, drop_prob)
+          if not isinstance(op2, Identity) :
+            h2 = drop_path(h2, drop_prob)
+        s = h1 + h2
+
+      else :
+        # changed for 3rd path
+        h1 = states[self._indices[3*i-1]]
+        h2 = states[self._indices[3*i]]
+        h3 = states[self._indices[3*i+1]]
+        op1 = self._ops[3*i-1]
+        op2 = self._ops[3*i]
+        op3 = self._ops[3*i+1]
+        h1 = op1(h1)
+        h2 = op2(h2)
+        h3 = op3(h3)
+        if self.training and drop_prob > 0. :
+          if not isinstance(op1, Identity) :
+            h1 = drop_path(h1, drop_prob)
+          if not isinstance(op2, Identity) :
+            h2 = drop_path(h2, drop_prob)
+          if not isinstance(op3, Identity) :
+            h3 = drop_path(h3, drop_prob)
+        s = h1 + h2 + h3
+        
       states += [s]
+      
     return torch.cat([states[i] for i in self._concat], dim=1)
 
 
